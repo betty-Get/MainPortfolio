4177098962e58052de153451ba16f7be95bfddae
bettyGpt <betelhem.getachew@girlpowertalk.com>
2024-11-30 17:15:59 +0300

    contact page added

--- my-app/src/app/components/contact.jsx
+++ my-app/src/app/components/contact.jsx
@@ -4,19 +4,22 @@ import React from "react";
 const Contact = () => {
   return (
     <section className="main_style pt-12">
-      <h2 className="title pb-5">
+      <h2 className="title pb-2 md:pb-5">
         Get to Know Me
         <hr className="line"></hr>
       </h2>
-      <div className="flex flex-col md:flex-row  ml-36">
-        <div className="w-full md:w-1/2">
-          <h2 className="mini_title pt-6">Let's Chat</h2>
-          <h2 className="mini_title">And Build something together</h2>
-          <Link href={""} className="border rounded-md shadow-xl">
+      <div className="flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2 mt-3 md:mt-16  ml-0 md:ml-12 lg:ml-36">
+          <h2 className="mini_title mb-2">Let's Chat</h2>
+          <h2 className="mini_title mb-8">And Build something together</h2>
+          <Link
+            href={""}
+            className="border px-6 py-3 rounded-full shadow-xl bg-secondaryViolet text-white"
+          >
             Download Resume
           </Link>
         </div>
-        <div className="w-full md:w-1/2">
+        <div className="w-full md:w-1/2 mt-10 md:mt-0">
           <form>
             <div className="flex flex-col">
               <label className="text-[16px] roboto pb-2">Full Name*</label>
@@ -28,7 +31,7 @@ const Contact = () => {
               />
             </div>
             <div className="flex flex-col py-3">
-              <label>Email*</label>
+              <label className="text-[16px] roboto pb-2">Email*</label>
               <input
                 type="email"
                 name="email"
@@ -37,7 +40,7 @@ const Contact = () => {
               />
             </div>
             <div className="flex flex-col">
-              <label>Message*</label>
+              <label className="text-[16px] roboto pb-2">Message*</label>
               <textarea
                 type="text"
                 name="message"
